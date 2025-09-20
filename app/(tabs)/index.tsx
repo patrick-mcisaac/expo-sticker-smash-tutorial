f93d652504bb743daa178fbdda0653d920585f96
Patrick McIsaac <patrick.mcisaac.dev@gmail.com>
2025-09-20 12:07:53 -0500

    added modal for emoji

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,13 @@
 import Button from "@/components/Button"
+import CircleButtons from "@/components/CircleButtons"
+import EmojiList from "@/components/EmojiList"
+import EmojiPicker from "@/components/EmojiPicker"
+import EmojiSticker from "@/components/EmojiSticker"
+import IconButton from "@/components/IconButton"
 import ImageViewer from "@/components/ImageViewer"
 import * as ImagePicker from "expo-image-picker"
 import { useState } from "react"
-import { StyleSheet, View } from "react-native"
+import { ImageSourcePropType, StyleSheet, View } from "react-native"
 
 const PlaceholderImage = require("@/assets/images/background-image.png")
 
@@ -10,6 +15,11 @@ export default function Index() {
     const [selectedImage, setSelectedImage] = useState<string | undefined>(
         undefined
     )
+    const [showAppOptions, setShowAppOptions] = useState<boolean>(false)
+    const [isModalVisible, setIsModalVisible] = useState<boolean>(false)
+    const [pickedEmoji, setPickedEmoji] = useState<
+        ImageSourcePropType | undefined
+    >(undefined)
 
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -20,9 +30,25 @@ export default function Index() {
 
         if (!result.canceled) {
             setSelectedImage(result.assets[0].uri)
+            setShowAppOptions(true)
         } else {
             alert("You did not select any image")
         }
+    }
+
+    const onReset = (): void => {
+        setShowAppOptions(false)
+    }
+
+    const onAddSticker = (): void => {
+        setIsModalVisible(true)
+    }
+    const onModalClose = (): void => {
+        setIsModalVisible(false)
+    }
+
+    const onSaveImageAsync = async (): Promise<void> => {
+        // we will implement this later
     }
 
     return (
@@ -32,15 +58,45 @@ export default function Index() {
                     imageSource={PlaceholderImage}
                     selectedImage={selectedImage}
                 />
+                {pickedEmoji && (
+                    <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+                )}
             </View>
-            <View style={styles.footerContainer}>
-                <Button
-                    label="Choose a photo"
-                    theme="primary"
-                    onPress={pickImageAsync}
+            {showAppOptions ? (
+                <View style={styles.optionsContainer}>
+                    <View style={styles.optionsRow}>
+                        <IconButton
+                            icon="refresh"
+                            label="Reset"
+                            onPress={onReset}
+                        />
+                        <CircleButtons onPress={onAddSticker} />
+                        <IconButton
+                            icon="save-alt"
+                            label="Save"
+                            onPress={onSaveImageAsync}
+                        />
+                    </View>
+                </View>
+            ) : (
+                <View style={styles.footerContainer}>
+                    <Button
+                        label="Choose a photo"
+                        theme="primary"
+                        onPress={pickImageAsync}
+                    />
+                    <Button
+                        onPress={() => setShowAppOptions(true)}
+                        label="Use this photo"
+                    />
+                </View>
+            )}
+            <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+                <EmojiList
+                    onSelect={setPickedEmoji}
+                    onCloseModal={onModalClose}
                 />
-                <Button label="Use this photo" />
-            </View>
+            </EmojiPicker>
         </View>
     )
 }
@@ -58,5 +114,13 @@ const styles = StyleSheet.create({
     footerContainer: {
         flex: 1 / 3,
         alignItems: "center"
+    },
+    optionsContainer: {
+        position: "absolute",
+        bottom: 80
+    },
+    optionsRow: {
+        alignItems: "center",
+        flexDirection: "row"
     }
 })
--- components/Button.tsx
+++ components/Button.tsx
@@ -40,10 +40,7 @@ export default function Button({ label, theme, onPress }: Props) {
     }
     return (
         <View style={styles.buttonContainer}>
-            <Pressable
-                style={styles.button}
-                onPress={() => alert("you pressed a button")}
-            >
+            <Pressable style={styles.button} onPress={onPress}>
                 <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
         </View>
